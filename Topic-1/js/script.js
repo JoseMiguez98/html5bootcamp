52fdcd3e5b7f369d73fada4da886aef3c27dece9
JoseMiguez98 <josemiguez184@gmail.com>
2019-05-15 05:41:16 -0300

    Module_1 - 3.1 - Created function that fetch data from server

--- Topic-1/js/script.js
+++ Topic-1/js/script.js
@@ -6,9 +6,27 @@ window.onload = function() {
         alert("Hi you clicked the button!");
     }
 
+    function fetchData() {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://api.icndb.com/jokes/random");
+        xhr.responseType = "json";
+        xhr.send();
+        
+        xhr.onload = function () {
+            if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+                console.log(xhr.response);
+            }
+            else {
+                console.log(`${xhr.status}: ${xhr.statusText} ${xhr.response}`)
+            }
+        }
+    }
+
     let sectionElem = document.querySelector(".section.hidden");
     let buttonElem = document.querySelector(".button.alert")
 
     sectionElem.classList.remove("hidden");
     buttonElem.addEventListener("click", showMessage);
+
+    fetchData();
 }
