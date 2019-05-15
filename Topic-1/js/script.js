820fae61fcf194821b3b0dcee0029407acc12b73
JoseMiguez98 <josemiguez184@gmail.com>
2019-05-15 04:31:07 -0300

    Module_1 - 2.* - Added button that show an alert message

--- Topic-1/js/script.js
+++ Topic-1/js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
 window.onload = function() {
+
+    function showMessage(){
+        alert("Hi you clicked the button!");
+    }
+
     let sectionElem = document.querySelector(".section.hidden");
+    let buttonElem = document.querySelector(".button.alert")
+
     sectionElem.classList.remove("hidden");
+    buttonElem.addEventListener("click", showMessage);
 }
