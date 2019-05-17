8e68d1a38c49583bb49b1bd832f36ab88c0033a8
JoseMiguez98 <josemiguez184@gmail.com>
2019-05-17 04:27:27 -0300

    Module_1 - 3.4 - Created generic AJAX function

--- Topic-1/js/script.js
+++ Topic-1/js/script.js
@@ -10,11 +10,10 @@ window.onload = function() {
         
         xhr.onload = function () {
             if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-                let section = document.getElementsByClassName("section")[0];
-                let h2Elem = document.createElement("p");
+                let paragraphElem = document.createElement("p");
                 let response = document.createTextNode(xhr.response.value.joke);
-                h2Elem.appendChild(response);
-                section.appendChild(h2Elem); 
+                paragraphElem.appendChild(response);
+                sectionElem.appendChild(paragraphElem); 
             }
             else {
                 console.log(`${xhr.status}: ${xhr.statusText} ${xhr.response}`);
@@ -22,9 +21,41 @@ window.onload = function() {
         }
     }
 
+    function fetchDataGeneric(config) {
+        return fetch(config.url, {
+            method: config.method,
+            headers: config.headers ? config.headers : Headers,
+        });
+    }
+
+    function getJoke() {
+        let promise = fetchDataGeneric(fetchConfig);
+        promise.then( (response) => {
+            if(!response.ok){
+                console.log(Error(response.statusText));
+                throw Error;
+            }
+            return response.json();
+        }).then( (jsonResponse) => {
+            let paragraphElem = document.createElement("p");
+            let response = document.createTextNode(jsonResponse.value.joke);
+            paragraphElem.appendChild(response);
+            console.log(sectionElem);
+            sectionElem.appendChild(paragraphElem); 
+        }).catch( (err) => {
+            console.log(err);
+        });
+    }
+
     let sectionElem = document.querySelector(".section.hidden");
     let buttonElem = document.querySelector(".button.alert");
+    let fetchConfig = {
+        url: "http://api.icndb.com/jokes/random",
+        method: "GET"
+    };
 
     sectionElem.classList.remove("hidden");
-    buttonElem.addEventListener("click", fetchData)
+    buttonElem.addEventListener("click", () => {
+        getJoke();
+    });
 }
