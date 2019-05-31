2708cf3e1bac5b00f2125de2eb5a2ec5e6e7c5d2
JoseMiguez98 <josemiguez184@gmail.com>
2019-05-31 06:57:06 -0300

    Module_4 - 5.* - Added canvas shapes and animations

--- Topic-4/js/script.js
+++ Topic-4/js/script.js
@@ -2,6 +2,16 @@ window.onload = () => {
     
     "use strict";
     
+    const saveButtonElem = document.getElementsByClassName("button save")[0];
+    const deleteButtonElem = document.getElementsByClassName("button delete")[0];
+    const textAreaElem = document.getElementById("txt-area");
+    const webSocket = new WebSocket("wss://echo.websocket.org/");
+    const webSocketContainerElem = document.getElementsByClassName("webSocket-info-container")[0];
+    const canvas = document.getElementsByClassName("myCanvas")[0];
+    const context = canvas.getContext("2d");
+    let db;
+    
+  
     // ======================================= Functions ====================================================//
     function addIndexedDBData(value) {
         const data = {"data": value};
@@ -38,15 +48,83 @@ window.onload = () => {
             }
         }
     })()
-    // ======================================================================================//
     
-    const saveButtonElem = document.getElementsByClassName("button save")[0];
-    const deleteButtonElem = document.getElementsByClassName("button delete")[0];
-    const textAreaElem = document.getElementById("txt-area");
-    const webSocket = new WebSocket("wss://echo.websocket.org/");
-    const webSocketContainerElem = document.getElementsByClassName("webSocket-info-container")[0];
-    let db;
+    ;(function drawRandom() {
+        const rectWidth = 500;
+        const rectHeight = canvas.height;
+        
+        //Background rectangle
+        context.beginPath();
+        context.fillStyle = getRandomColor();
+        context.strokeRect(0, 0, rectWidth, rectHeight);
+        context.fillRect(0, 0, rectWidth, rectHeight);
+        
+        //Circle
+        context.lineWidth = Math.floor(Math.random() * 10);
+        context.arc(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight), 50, 0, 2 * Math.PI);
+        context.stroke();
+        
+        //Random Strokes
+        context.lineWidth = Math.floor(Math.random() * 10);
+        context.strokeStyle = getRandomColor();
+        context.moveTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.lineTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.moveTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.lineTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.moveTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.lineTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.moveTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.lineTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.moveTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.lineTo(getRandomStrokePosX(rectWidth), getRandomStrokePosY(rectHeight));
+        context.stroke();
+    })();
     
+    function getRandomColor() {
+        const pixel = {
+            r: Math.floor(Math.random() * 256),
+            g: Math.floor(Math.random() * 256),
+            b: Math.floor(Math.random() * 256)
+        }
+        
+        return `rgb(${pixel.r}, ${pixel.g}, ${pixel.b})`;
+    }
+    
+    function getRandomStrokePosX(rectWidth) {
+        return Math.floor(Math.random() * rectWidth+1);
+    }
+    
+    function getRandomStrokePosY(rectHeight) {
+        return Math.floor(Math.random() * rectHeight+1);
+    }
+    
+
+    //Comment this function if you wanna see drawRandom() function in action
+    //because the clearRect() sentence clear all the canvas to perform animation
+    //and also clean the drawRandom() result, i tried to find a way to show both
+    //but i have no exit.
+    (function animateRectangle(rect ,isGoingDown) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.fillStyle = "#000";
+        context.strokeRect(rect.posX, rect.posY, rect.width, rect.height);
+        if(rect.posY < canvas.height-rect.height && isGoingDown){
+            rect.posY += 7;
+        } else {
+            isGoingDown = false;
+            
+            if(rect.posY > 0) {
+                rect.posY -= 7;
+            } else {
+                isGoingDown = true;
+                rect.posY += 7;
+            }
+        }
+
+        window.requestAnimationFrame( () => {
+            animateRectangle(rect ,isGoingDown);
+        });
+        
+    })({posX : 600,posY : 0, width: 300,height: 100}, true);
     
     //============================ Storage Events Handlers ===========================//
     saveButtonElem.onclick = () => {
@@ -144,29 +222,29 @@ window.onload = () => {
         }
         evt.target.classList.remove("hover");
     }
-
+    
     //==============================================================================//
-
+    
     //=========================== WebSocket Events Handlers =======================//
-
+    
     webSocket.onopen = evt => {
         const paragraphElem = document.createElement("p");
         paragraphElem.appendChild(document.createTextNode("Web Socket Connection Opened!"));
         webSocketContainerElem.appendChild(paragraphElem);
-
+        
         webSocket.send("Testing Web Socket Connection!");
     }
-
+    
     webSocket.onerror = evt => {
         const paragraphElem = document.createElement("p");
         paragraphElem.appendChild(document.createTextNode("An error ocurred opening Web Socket"));
         paragraphElem.classList.add("error");
         webSocketContainerElem.appendChild(paragraphElem);
     }
-
+    
     webSocket.onmessage = evt => {
         const paragraphElem = document.createElement("p");
-        paragraphElem.appendChild(document.createTextNode("Message recieved form the Server: " + evt.data));
+        paragraphElem.appendChild(document.createTextNode("Message recieved from the Server: " + evt.data));
         webSocketContainerElem.appendChild(paragraphElem);
     }
 }
